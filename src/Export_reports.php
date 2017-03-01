3b3ec561c24dae1cd2ab535a31925ace717c4547
Christian Cruz <ccruz.ga17@gmail.com>
2017-03-01 14:54:09 -0600

    Tdo: Prepare library for composer

---
+++ src/Export_reports.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class Export_reports, you can read and excel file
+ *
+ * This class can extract information from a target or multiple targets and each of them with one or more values
+ *
+ * @category   CCRUZ Export Reports
+ * @package    ccruz17/export_reports_excel
+ * @author     Christian Cruz Garrido <ccruz.ga17@gmail.com>
+ * @copyright  2016-2017 CCRUZ
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt  GNU GENERAL PUBLIC LICENSE V3
+ * @link       https://github.com/ccruz17/export_reports_excel
+ */
+namespace ccruz17;
+
+class Export_reports
+{
+    public function make($initial_data, $headers, $data, $sections = true) {
+
+    }
+}
